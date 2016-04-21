d129df277d4a3b16dc44e3178fde3a057c088175
Richard Littauer <richard.littauer@gmail.com>
2016-04-21 14:48:30 -0400

    Added coutnries

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const _ = require('lodash')
 
 const states = require('./states.json')
 
-console.log('Total visited: ', _.filter(states, {visited: true}).length)
+console.log('States visited: ', _.filter(states, {visited: true}).length)
 console.log('Lived in: ', _.filter(states, {lived: true}).length)
 console.log('High-pointed: ', _.filter(states, {"high-pointed": true}).length)
+
+const countries = require('./countries.json')
+
+console.log('')
+console.log('Countries visited: ', _.filter(countries, {visited: true}).length)
+console.log('Lived in: ', _.filter(countries, {lived: true}).length)
+console.log('High-pointed: ', _.filter(countries, {"high-pointed": true}).length)
