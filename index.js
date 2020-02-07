928d63b8c37afbb1880b1cf67cc8b7cbaa271c24
Richard Littauer <richard+github@burntfen.com>
2020-02-07 09:36:50 -0500

    I been more places

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const states = require('./states.json')
 console.log('States visited: ', _.filter(states, {visited: true}).length)
 console.log('Lived in: ', _.filter(states, {lived: true}).length)
 console.log('High-pointed: ', _.filter(states, {"high-pointed": true}).length)
+console.log('Driven only: ', _.filter(states, {"drive-only": true}).length)
 
 const countries = require('./countries.json')
 
@@ -12,3 +13,4 @@ console.log('')
 console.log('Countries visited: ', _.filter(countries, {visited: true}).length)
 console.log('Lived in: ', _.filter(countries, {lived: true}).length)
 console.log('High-pointed: ', _.filter(countries, {"high-pointed": true}).length)
+console.log('Driven only: ', _.filter(countries, {"drive-only": true}).length)
